7ff4c7c23a66409565c177254b0c821d83810562
Michael Moore <michaelrmoore27@gmail.com>
2017-01-02 11:42:29 -0500

    added tile algorithm, Quote has background now

--- Cavestory/level.h
+++ Cavestory/level.h
@@ -2,7 +2,13 @@
 #ifndef LEVEL_H
 #define LEVEL_H
 #include <string>
+#include <vector>
 #include "globals.h"
+#include "tile.h"
+
+struct SDL_Rect;
+struct Tileset;
+
 
 class Graphics;
 struct SDL_Texture;
@@ -18,12 +24,32 @@ private:
 	std::string _mapName;
 	Vector2 _spawnPoint;
 	Vector2 _size;
+	Vector2 _tileSize;
+
 	SDL_Texture* _backgroundTexture;
+
+	std::vector<Tile> _tileList;
+	std::vector<Tileset> _tilesets;
+
 	
 	//Loads a map
 	void loadMap(std::string mapNAme, Graphics &graphics);
 
 
 };
+
+//Tileset structure
+struct Tileset {
+	SDL_Texture* Texture;
+	int FirstGid;
+	Tileset() {
+		this->FirstGid = -1;
+	}
+	Tileset(SDL_Texture* texture, int firstGid) {
+		this->Texture = texture;
+		this->FirstGid = firstGid;
+	}
+};
+
 #endif // !LEVEL_H
 
